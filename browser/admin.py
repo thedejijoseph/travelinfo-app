db0d8bda29af905150f640b5f59d55ab34ba7c51
Deji <wrecodde@gmail.com>
2021-03-09 00:07:00 +0100

    first look at app/db scehmas

--- browser/admin.py
+++ browser/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import State, Terminal
+
+admin.site.register(State)
+admin.site.register(Terminal)
--- browser/models.py
+++ browser/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class State(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Terminal(models.Model):
+    name = models.CharField(max_length=300)
+    AIR = "air"
+    ROAD = "road"
+    RAIL = "rail"
+    SUPPORTED_MODES = (
+        (AIR, 'Air'),
+        (ROAD, 'Road'),
+        (RAIL, 'Rail')
+    )
+    mode = models.CharField(max_length=12, choices=SUPPORTED_MODES)
+    state = models.ForeignKey('State', on_delete=models.PROTECT)
+    # set to protect cause states should not be deleted easily
+    description = models.TextField()
+    dest_terminals = models.ManyToManyField('self', blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.state}"
