75eebe3947edafc0418992eeb6160eac72607966
Deji <wrecodde@gmail.com>
2021-03-09 23:01:39 +0100

    editing terminals work

--- browser/urls.py
+++ browser/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('terminals/new', views.terminals_new),
-    path('terminals/id/edit', views.terminals_edit),
+    path('terminals/<uuid:terminal_id>/edit', views.terminals_edit),
     path('terminals/<uuid:terminal_id>', views.terminals_page),
     path('terminals', views.terminals_all),
     path('states/<str:state_id>', views.states_page),
--- browser/views.py
+++ browser/views.py
@@ -13,29 +13,63 @@ def index(request):
     return render(request, 'browser/index.html', context)
 
 def terminals_new(request):
-    # return HttpResponseRedirect('/')
     if request.method == 'POST':
         form = TerminalForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
             terminal = Terminal()
-            terminal.name = data['name']
-            terminal.mode = data['mode']
-            terminal.description = data['description']
-            terminal.state = State.objects.get(state_id=data['state'])
-            terminal.save() # saving here to generate id. id is needed to create manytomany relationship
-            dest_terminals = [Terminal.objects.get(terminal_id=terminal_id) for terminal_id in data['dest_terminals']]
-            for dest_terminal in dest_terminals:
-                terminal.dest_terminals.add(dest_terminal)
             terminal.save()
-            return HttpResponseRedirect('/')
+            try:
+                terminal.name = data['name']
+                terminal.mode = data['mode']
+                terminal.description = data['description']
+                terminal.state = State.objects.get(state_id=data['state'])
+
+                dest_terminals = [Terminal.objects.get(terminal_id=terminal_id) for terminal_id in data['dest_terminals']]
+                for dest_terminal in dest_terminals:
+                    terminal.dest_terminals.add(dest_terminal)
+                
+                terminal.save()
+                return HttpResponseRedirect('/')
+            except:
+                # ensure incomplete records are not written to db
+                terminal.delete()
     else:
         form = TerminalForm()
 
     return render(request, 'browser/terminals_new.html', {'form': form})
 
-def terminals_edit(request):
-    return render(request, 'browser/terminals_edit.html')
+def terminals_edit(request, terminal_id):
+    terminal = Terminal.objects.get(terminal_id=terminal_id)
+
+    if request.method == 'POST':
+        form = TerminalForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            terminal.name = data['name']
+            terminal.mode = data['mode']
+            terminal.description = data['description']
+            terminal.state = State.objects.get(state_id=data['state'])
+
+            terminal.dest_terminals.clear() # avoid incremental addition
+            dest_terminals = [Terminal.objects.get(terminal_id=terminal_id) for terminal_id in data['dest_terminals']]
+            for dest_terminal in dest_terminals:
+                terminal.dest_terminals.add(dest_terminal)
+            
+            terminal.save()
+            return HttpResponseRedirect(f'/terminals/{terminal_id}')
+    else:
+        existing_data = {
+            'name': terminal.name,
+            'mode': terminal.mode,
+            'state': terminal.state.state_id,
+            'description': terminal.description,
+            'dest_terminals': [dest_terminal.terminal_id for dest_terminal in terminal.dest_terminals.all()]
+        }
+        form = TerminalForm(initial=existing_data)
+    
+    context = {'terminal': terminal, 'form': form}
+    return render(request, 'browser/terminals_edit.html', context)
 
 def terminals_all(request):
     terminals = Terminal.objects.all()
